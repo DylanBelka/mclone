e8c224de298f2585249d2b94459af6e40fc11586
dylanbelka <dylan.belka@gmail.com>
2017-07-16 22:30:37 -0400

    Move Face to block, Block can calc face index

--- Block.h
+++ Block.h
@@ -1,27 +1,44 @@
 #ifndef BLOCK_H
 #define BLOCK_H
 
-#include <glew.h>
-
 #include <glm.hpp>
-#include <gtc/matrix_transform.hpp>
 
 #include "Shader.h"
 #include "stb_image.h"
+
+namespace Face {
+	enum FaceIndex
+	{
+		FRONT,
+		BACK,
+		LEFT,
+		RIGHT,
+		BOTTOM,
+		TOP,
+
+		NUM_FACES
+	};
+}
 
 class Block
 {
 public:
 	Block();
+	/*
+		returns the offset from the block type's base texture to the unique face texture
+		or
+		returns 0 if the face is not unique
+	*/
+	int uniqueFaceIndexOffset(Face::FaceIndex face); 
 
 	enum BlockType
 	{
 		AIR,
-		STONE,
-		DIRT
+		GRASS,
+		DIRT,
 	};
 
-	BlockType type; 
+	BlockType type;
 };
 
 #endif // BLOCK_H
--- block.cpp
+++ block.cpp
@@ -4,3 +4,23 @@ Block::Block()
 {
 	type = Block::AIR;
 }
+
+int Block::uniqueFaceIndexOffset(Face::FaceIndex face)
+{
+	if (face == Face::FaceIndex::TOP) // grass,
+	{
+		if (type == Block::GRASS)
+		{
+			return 1; // grass block bottom is 1 atlas coord away from base
+		}
+	}
+	else if (face == Face::FaceIndex::BOTTOM) // grass,
+	{
+		if (type == Block::GRASS)
+		{
+			return 2; // grass block bottom is 2 atlas coords away from base
+		}
+	}
+
+	return 0;
+}
