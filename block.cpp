c2589e6275ee184884415b55b30fcb66009c96c6
dylanbelka <dylan.belka@gmail.com>
2017-08-07 01:43:46 -0400

    Add new blocks, string to BlockType conversion

--- block.cpp
+++ block.cpp
@@ -7,20 +7,57 @@ Block::Block()
 
 int Block::uniqueFaceIndexOffset(Face::FaceIndex face)
 {
-	if (face == Face::FaceIndex::TOP) // grass,
+	if (face == Face::FaceIndex::TOP) // grass, wood
 	{
 		if (type == Block::GRASS)
 		{
 			return 1; // grass block bottom is 1 atlas coord away from base
 		}
+		if (type == Block::WOOD)
+		{
+			return 1;
+		}
 	}
-	else if (face == Face::FaceIndex::BOTTOM) // grass,
+	else if (face == Face::FaceIndex::BOTTOM) // grass, wood
 	{
 		if (type == Block::GRASS)
 		{
 			return 2; // grass block bottom is 2 atlas coords away from base
 		}
+		if (type == Block::WOOD)
+		{
+			return 1;
+		}
 	}
 
 	return 0;
+}
+
+Block::BlockType Block::strToBlockType(const std::string& str)
+{
+	if (str == "AIR")
+	{
+		return Block::AIR;
+	}
+	if (str == "GRASS")
+	{
+		return Block::GRASS;
+	}
+	if (str == "DIRT")
+	{
+		return Block::DIRT;
+	}
+	if (str == "STONE")
+	{
+		return Block::STONE;
+	}
+	if (str == "WOOD")
+	{
+		return Block::WOOD;
+	}
+	if (str == "LEAVES")
+	{
+		return Block::LEAVES;
+	}
+	return Block::AIR;
 }
